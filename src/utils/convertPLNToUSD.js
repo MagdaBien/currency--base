265e31f27b2957103caedea6a94d312d2c5caaa0
Magdalena Bień <bien@magda.krakow.pl>
2024-01-11 12:36:06 +0100

    adding basic tests

---
+++ src/utils/convertPLNToUSD.js
@@ -0,0 +1,23 @@
+export const convertPLNToUSD = (PLN) => {
+
+  if (typeof(PLN) === "string" || PLN === '') { 
+    return NaN; 
+  }
+
+  if (typeof(PLN) !== "number") { 
+    return "Error"; 
+  }  
+
+  if (PLN < 0 ) { 
+    PLN = 0; 
+  } 
+
+  const PLNtoUSD = PLN / 3.5;
+  
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+}
